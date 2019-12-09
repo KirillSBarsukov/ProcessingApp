72ded8d0ba2fac02f95975dc16cf37912f9505b8
deep98parmar8 <55211433+deep98parmar8@users.noreply.github.com>
2019-12-09 13:38:40 -0500

    Adding a new model

---
+++ ProcessingApp/Models/Tenenants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingApp.Models
+{
+    public class Tenenants
+    {
+        [key]
+
+        public virtual int TenenatsId { get; set; }
+
+        public virtual String TenantReviews { get; set; }
+
+        public virtual String Ambiance { get; set; }
+
+
+    }
+}
---
+++ ProcessingApp/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProcessingApp.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
